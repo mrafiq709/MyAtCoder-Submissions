a57c97edacc8ba2cab020f830d3304733790525e
AutoCloneBot <acautoclone@bot.com>
2025-03-13 14:46:35 +0000

    Added my codes from AtCoder

---
+++ abc044/abc044_b.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string str;
+    cin >> str;
+    vector <int> arr(123);
+    for(int i = 0; i < str.length(); i++) {
+        arr[str[i]]++;
+    }
+    
+    for(int i = 97; i <= 122; i++) {
+        // cout << arr[i] << " ";
+        if(arr[i] % 2 == 1) {
+            cout << "No" << endl;
+            return 0;
+        }
+    }
+    cout << "Yes" << endl;
+    return 0;
+}
